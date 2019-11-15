5aa1da11e6d6103d5693e39ebd2b6076fdaa251b
Jerry Shi <jerryshi25@gmail.com>
2019-11-15 03:33:19 +0000

    untested version of everything

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,4 +13,21 @@
 #
 
 class Photo < ApplicationRecord
+    def poster
+        return User.where({ :id => self.owner_id}).first
+    end
+    def comments
+        return Comment.where({ :photo_id => self.id})
+    end
+    def likes
+        return Like.where({ :photo_id => self.id})
+    end
+    def fans
+        fan_ids = Like.where({ :photo_id => self.id}).pluck(:fan_id)
+        return User.where({ :id => fan_ids})
+    end
+    def fans
+        fan_ids = Like.where({ :photo_id => self.id}).pluck(:fan_id)
+        return User.where({ :id => fan_ids}).to_sentence
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,49 @@
 #
 
 class User < ApplicationRecord
+    def comments
+        return Comment.where({ :author_id => self.id})
+    end
+    def own_photos
+        return Photo.where({ :owner_id => self.id})
+    end
+    def likes
+        return Like.where({fan_id => self.id})
+    end
+    def liked_photos
+        photo_ids = Like.where({fan_id => self.id}).pluck(:photo_id)
+        return Photo.where({ :id => photo_ids})
+    end
+    def commented_photos
+        photo_ids = Comment.where({ :author_id => self.id}).pluck(:photo_id)
+        return Photo.where({ :id => photo_ids})
+    end
+    def sent_follow_requests
+        return Follow_requests.where({ :sender_id => self.id})
+    end
+    def received_follow_requests
+        return Follow_requests.where({ :recipient_id => self.id})
+    end
+    def accepted_sent_follow_requests
+        return Follow_requests.where({ :sender_id => self.id}).where({ :status => "accepted"})
+    end
+    def accepted_received_follow_requests
+        return Follow_requests.where({ :recipient_id => self.id}).where({ :status => "accepted"})
+    end
+    def following
+        following_ids = Follow_requests.where({ :sender_id => self.id}).where({ :status => "accepted"}).pluck(:recipient_id)
+        return User.where({ :id => following_ids})
+    end
+    def followers
+        follower_ids = Follow_requests.where({ :recipient_id => self.id}).where({ :status => "accepted"}).pluck(:sender_id)
+        return User.where({ :id => follower_ids})
+    end
+    def feed
+        following_ids = Follow_requests.where({ :sender_id => self.id}).where({ :status => "accepted"}).pluck(:recipient_id)
+        return Photo.where({ :owner_id => following_ids})
+    end
+    def discover
+        following_ids = Follow_requests.where({ :sender_id => self.id}).where({ :status => "accepted"}).pluck(:recipient_id)
+        following_likes = Like.where({ :fan_id => following_ids}).pluck(:photo_id)
+        return Photo.where({ :id => following_likes})
 end
